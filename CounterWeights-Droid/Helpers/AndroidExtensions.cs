8bb9e3c2de464f445768c3e240fa4abdfd316d8f
Christian Capil <christian.capil@gmail.com>
2015-02-18 09:34:14 -0800

    Added extension class for utility methods

---
+++ CounterWeights-Droid/Helpers/AndroidExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CounterWeightsDroid
+{
+	public static class AndroidExtensions
+	{
+		public static bool IsMaterial {
+			get {
+				return Android.OS.Build.VERSION.SdkInt == Android.OS.BuildVersionCodes.Lollipop;
+			}
+		}
+	}
+}
+
